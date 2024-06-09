340424896d52043c3a190c8121a45ebe603ab211
akshad07 <akshadpardeshi07@gmail.com>
2024-06-09 13:13:58 +0530

    Done

--- csv_analysis_project/csv_analysis_project/settings.py
+++ csv_analysis_project/csv_analysis_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,9 +113,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Ensure media files handling
+# Add or update these settings
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
